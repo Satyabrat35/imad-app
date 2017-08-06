419e84f080d987a19c9c530e9fa0c51d0ad72037
Satyabrat Bhol <satyabrat35@gmail.com>
2017-08-06 20:11:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.get('/article-one',function(req,res){
 });
 
 app.get('/article-two',function(req,res){
-   res.send('Article-two will be served') ;
+   res.sendFile(path.join(__dirname, 'ui', 'Article-two.html'));
 });
 
 app.get('/article-three',function(req,res){
-   res.send('Article-three will be served'); 
+   res.sendFile(path.join(__dirname, 'ui', 'Article-three.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
