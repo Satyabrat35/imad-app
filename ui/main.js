cd491118ae14a8c91a25013532b1ed44c2d8ef4c
Satyabrat Bhol <satyabrat35@gmail.com>
2017-08-09 23:52:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,11 +25,25 @@ var name = nameinput.value;
 var submit  = document.getElementById('subbtn');
 submit.onclick = function() {
     
-    var names = ['aman' , 'aditya' , 'deepak' ];
-    var list = '';
-    for(var i=0 ; i < names.length ; i++){
-        list += '<li>' + names[i] + '</li>';
+    var req = new XMLHttpRequest();
+req.onreadystatechange = function(){
+    if(req.readyState === XMLHttpRequest.DONE){
+        
+        if(req.status === 200){
+            var names = req.responseText;
+            names=JSON.parse(names); //convert string back into object
+             var list = '';
+            for(var i=0 ; i < names.length ; i++){
+              list += '<li>' + names[i] + '</li>';
+          }
+          var ul = document.getElementById('nameslist');
+           ul.innerHTML = list;
+        }
     }
-var ul = document.getElementById('nameslist');
-ul.innerHTML = list;
+};
+    
+   req.open('GET','http://satyabrat35.imad.hasura-app.io/submit-name?name='+ name ,true);
+   req.send(null);
+    
+    
 };
