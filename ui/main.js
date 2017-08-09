206dc23c7aff97e4171cf5d5ecc96c07462af73c
Satyabrat Bhol <satyabrat35@gmail.com>
2017-08-09 20:13:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,14 +20,14 @@ req.onreadystatechange = function(){
     
 };
 
-var nameinut = document.getElementById('name');
+var nameinput = document.getElementById('name');
 var name = nameinput.value;
 var submit  = document.getElementById('subbtn');
 submit.onclick = function() {
     
     var names = ['aman','aditya','deepak','ritesh'];
     var list = '';
-    for(var i=0;i<names.length;i++){
+    for(var i=0;i < names.length ; i++){
         list = '<li>' + names[i] + '</li>';
     }
 var ul = document.getElementById('nameslist');
