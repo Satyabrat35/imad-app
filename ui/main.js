101211711342f115cfa4ee642be1c05c9769667f
Satyabrat Bhol <satyabrat35@gmail.com>
2017-08-09 18:38:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,13 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
 
+var counter = 0;
+
+button.onclick = function() {
+    
+    
+    
+    var span = document.getElementById('count');
+    counter = counter + 1;
+    span.innerHTML = counter.toString();
+    
+};
