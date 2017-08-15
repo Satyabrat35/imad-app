2329df70bb5750cc2ecf6dfb73a64ec59d5fa429
Satyabrat Bhol <satyabrat35@gmail.com>
2017-08-15 20:56:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.get('/counter',function(req,res){
 app.get('/articles/:articlename',function(req,res){
     var articlenamez  = req.params.articlename;
     
-    pool.query("SELECT * FROM article WHERE title = '" + req.params.articlename + "'", function(err,result){
+    pool.query("SELECT * FROM article WHERE title = $1",[req.params.articlename] , function(err,result){
        if(err){
            res.status(500).send(err.toString);
        } 
