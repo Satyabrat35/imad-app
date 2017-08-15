86c0eb33d8f07c1cf956c5bd33649a49282f0274
Satyabrat Bhol <satyabrat35@gmail.com>
2017-08-15 20:11:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,9 +107,25 @@ app.get('/counter',function(req,res){
 });
 
 
-app.get('/:articlename',function(req,res){
+app.get('/articles/:articlename',function(req,res){
     var articlenamez  = req.params.articlename;
-res.send(createtemplate(articles[articlenamez]));
+    
+    pool.query("SELECT * FROM article WHERE title = '" + req.params.articlename + "'", function(err,result){
+       if(err){
+           res.status(500).send(err.toString);
+       } 
+       else
+       {
+           if(result.rows.length === 0){
+               res.status(404).send('Article not found');
+           }
+           else{
+               var articleData = result.rows[0];
+               res.send(createtemplate(articles[articlenamez]));
+           }
+       }
+    });
+
 });
 
 
