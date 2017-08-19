6f6c14e2cf58ec4c462dd792969540d7a72acba9
Satyabrat Bhol <satyabrat35@gmail.com>
2017-08-19 15:40:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,24 +1,3 @@
-var button = document.getElementById('counter');
-
-
-button.onclick = function() {
-    
-var req = new XMLHttpRequest();
-req.onreadystatechange = function(){
-    if(req.readyState === XMLHttpRequest.DONE){
-        
-        if(req.status === 200){
-            var counter = req.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-        }
-    }
-};
-    
-   req.open('GET','http://satyabrat35.imad.hasura-app.io/counter',true);
-   req.send(null);
-    
-};
 
 
 var submit  = document.getElementById('subbtn');
@@ -43,7 +22,7 @@ req.onreadystatechange = function(){
 var nameinput = document.getElementById('name');
 var name = nameinput.value;
     // the value must be refreshed before submitting
-   req.open('GET','http://satyabrat35.imad.hasura-app.io/submit-name?name='+ name ,true);
+   req.open('POST','http://satyabrat35.imad.hasura-app.io/submit-name?name='+ name ,true);
    req.send(null);
     
     
